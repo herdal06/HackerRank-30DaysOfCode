0d123e8de5b45a7c9a4bb18c2423d0a7f2ce3f6f
herdal06 <h.erdal721@gmail.com>
2022-03-09 23:08:23 +0300

    day5

---
+++ Day5_Loops.cpp
@@ -0,0 +1,13 @@
+int main()
+{
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+    
+    for(int i = 1; i <= 10; i++) 
+        cout << n << " x " << i << " = " << n*i << endl;
+    
+
+    return 0;
+}
