018e6a0c1022a104cd63fe2243d3959c679f77e1
herdal06 <h.erdal721@gmail.com>
2022-03-09 22:49:31 +0300

    day3

---
+++ Day3_Intro-to-Conditional-Statements.cpp
@@ -0,0 +1,20 @@
+int main()
+{
+    string N_temp;
+    getline(cin, N_temp);
+
+    int N = stoi(ltrim(rtrim(N_temp)));
+    
+    if(N % 2 == 1)
+        cout << "Weird" << endl;
+    else {
+        if(N >= 2 && N <= 5)
+            cout << "Not Weird" << endl;
+        else if(N >= 6 && N <= 20)
+            cout << "Weird" << endl;
+        else if(N > 20)
+            cout << "Not Weird" << endl;
+    }
+    
+    return 0;
+}
