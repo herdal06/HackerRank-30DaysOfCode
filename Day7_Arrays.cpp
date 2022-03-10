71e06cdd165ece6b1a30683d624e089524aaca05
herdal06 <h.erdal721@gmail.com>
2022-03-10 15:51:41 +0300

    Day7

---
+++ Day7_Arrays.cpp
@@ -0,0 +1,23 @@
+int main()
+{
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    string arr_temp_temp;
+    getline(cin, arr_temp_temp);
+
+    vector<string> arr_temp = split(rtrim(arr_temp_temp));
+
+    vector<int> arr(n);
+
+    for (int i = 0; i < n; i++) {
+        int arr_item = stoi(arr_temp[i]);
+
+        arr[i] = arr_item;
+    }
+    for(int i = arr.size() - 1; i >= 0; i--)
+        cout << arr[i] << " ";
+    return 0;
+}
